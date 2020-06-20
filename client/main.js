8bc84c676649c2da9d1372589c5961b37f24af2a
Saurabh <saurabhh404@gmail.com>
2020-06-20 23:48:48 +0530

    second commit, week2 completed, Modal error unresolved

--- client/main.js
+++ client/main.js
@@ -5,54 +5,81 @@ import {
   ReactiveVar
 } from 'meteor/reactive-var';
 
+import {
+  Mongo
+} from 'meteor/mongo'
+
 import './main.html';
 
+Images = new Mongo.Collection("images")
 
-if (Meteor.isClient) {
-  var img_data = [{
-    img_src: "/victor.jpg",
-    img_alt: "Victor with his puppy doll"
-  }, {
-    img_src: "/victor2.jpg",
-    img_alt: "victor waiting for ya"
-  }, {
-    img_src: "/yuri.jpg",
-    img_alt: "yuri is sad"
-  }, {
-    img_src: "ready.png",
-    img_alt: "the couple is ready for the finale!"
-  }]
-  Template.myImage.helpers({
-    images: img_data
-  });
-  Template.myImage.events({
-    'click .js-image':function(event){
-      $(event.target).css("width", "30%")
+// var img_data = [{
+//   img_src: "/victor.jpg",
+//   img_alt: "Victor with his puppy doll"
+// }, {
+//   img_src: "/victor2.jpg",
+//   img_alt: "victor waiting for ya"
+// }, {
+//   img_src: "/yuri.jpg",
+//   img_alt: "yuri is sad"
+// }, {
+//   img_src: "ready.png",
+//   img_alt: "the couple is ready for the finale!"
+// }]
+
+// Template.myImage.helpers({
+//   images: img_data
+// });
+
+Template.myImage.helpers({
+  images: Images.find({}, {
+    sort: {
+      createdOn: -1,
+      rating: -1,
     }
-  });
-}
+  })
 
-if (Meteor.isServer) {
-  console.log("I am the server");
-}
-/*
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+})
 
-
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
+Template.myImage.events({
+  'click .js-del-image': function(event) {
+    var image_id = this._id
+    console.log(image_id);
+    $('#' + image_id).hide('slow', function() {
+      Images.remove({
+        "_id": image_id
+      })
+    })
   },
+  'click .js-rate-image': function(event) {
+    var rating = $(event.currentTarget).data('userrating')
+    var image_id = this.id
+    Images.update({
+      "_id": image_id
+    }, {
+      $set: {
+        'rating': rating
+      }
+    })
+  },
+  'click js-show-image-form':function(event){
+    $("#imageFormModal").modal('show')
+
+  }
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
-*/
+Template.imageAddForm.events({
+  'submit .js-add-image': function(event) {
+    var img_src = event.target.img_src.value;
+    var img_alt = event.target.img_alt.value;
+    console.log(img_src + "\n" + img_alt)
+
+    Images.insert({
+      img_src: img_src,
+      img_alt: img_alt,
+      createdOn: new Date()
+    })
+
+    return false
+  }
+})
--- server/main.js
+++ server/main.js
@@ -1,5 +1,36 @@
-import { Meteor } from 'meteor/meteor';
+import {
+  Meteor
+} from 'meteor/meteor';
+import {
+  Mongo
+} from 'meteor/mongo'
+
+Images = new Mongo.Collection("images")
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  if (Images.find().count() == 0) {
+    for (var i = 1; i < 23; i++) {
+      Images.insert({
+        img_src: "img_" + i + ".jpg",
+        img_alt: "img number" + i
+      })
+    }
+    Images.insert({
+      img_src: "/victor.jpg",
+      img_alt: "Victor with his puppy doll"
+    })
+    Images.insert({
+      img_src: "/victor2.jpg",
+      img_alt: "victor waiting for ya"
+    })
+    Images.insert({
+      img_src: "/yuri.jpg",
+      img_alt: "yuri is sad"
+    })
+    Images.insert({
+      img_src: "ready.png",
+      img_alt: "the couple is ready for the finale!"
+    })
+    console.log("main.js - server says: " + Images.find().count());
+  }
 });
